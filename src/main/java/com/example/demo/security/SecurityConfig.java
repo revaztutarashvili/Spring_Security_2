eac194b5acb6f193d18635d05e9294f7381bb50f
revaztutarashvili <generationofjeans@gmail.com>
2025-08-05 00:20:21 +0400

    add config

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -1,4 +1,23 @@
 package com.example.demo.security;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Configuration
 public class SecurityConfig {
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity,
+    @Qualifier("UsernameAuthenticationFilter")OncePerRequestFilter authenticationProcessingFilter) throws Exception{
+        httpSecurity
+                .addFilterBefore(authenticationProcessingFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(x ->x.requestMatchers("/login").permitAll().anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable);
+        return httpSecurity.build();
+    }
 }
